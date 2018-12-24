91fd3eea219b4f640e48bde843f4cfe436758517
Chi0323 <zx0918884233@gmail.com>
2018-12-24 15:28:35 +0800

    12月24日

--- blog/article/admin.py
+++ blog/article/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from article.models import Article, Comment
+
+class CommentModelAdmin(admin.ModelAdmin):
+    list_display = ['article', 'content']
+    list_display_links = ['article']
+    list_filter = ['article', 'content']
+ 
+    class Meta:
+        model = Comment
+
+admin.site.register(Article)
+admin.site.register(Comment, CommentModelAdmin)
