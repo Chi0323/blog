86987d247f919a4574b7d64518d4417662846c1d
Chi0323 <zx0918884233@gmail.com>
2018-12-27 21:20:16 +0800

    第七章

--- blog/article/admin.py
+++ blog/article/admin.py
@@ -6,6 +6,8 @@ class CommentModelAdmin(admin.ModelAdmin):
     list_display = ['article', 'content']
     list_display_links = ['article']
     list_filter = ['article', 'content']
+    search_fields = ['content']
+    list_editable = ['content']
  
     class Meta:
         model = Comment
